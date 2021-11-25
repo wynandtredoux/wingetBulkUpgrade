8642823068c6c97fb108c6bc9e7a04c58ce9957e
Wynand Tredoux <wynand.tredoux@ucalgary.ca>
2021-11-25 16:34:45 -0700

    Create winBulk.py

---
+++ winBulk.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentParser
+from os import system
+
+# take in a list of packages to upgrade using "winget --upgrade <packages>"
+def winBulk(packages):
+    #print(f'\nPackages to be upgraded: {", ".join(packages)}')
+    # for each package
+    for package in packages:
+        print(f'\nUpgrading {package}...')  # print which package is being upgraded
+        system(f'cmd /c "winget upgrade \"{package}\""')  # winget upgrade
+    return
+
+
+if __name__ == '__main__':
+    # parse command line arguments
+    parser = ArgumentParser(description='Update multiple winget packages')
+    parser.add_argument('packages'
+                        , help='A list of package names, comma delimited', type=str)
+    args = parser.parse_args()
+    winBulk(args.packages.split(','))  # split argument string into list of package names
